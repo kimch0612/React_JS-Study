7b3df5400e28adaed9281d4538525b24261ae46f
Changhwan Kim <kimch061279@gmail.com>
2023-02-14 09:02:42 +0900

    Update App.js: Add Main Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+    <MainContainer>
+      <LeftContainer>
+          <Left />
+      </LeftContainer>
+      
+      <RightContainer>
+          <Right />
+      </RightContainer>
+    </MainContainer>
   );
 }
+
+const MainContainer = styled.div`
+  width: 100%
+  hight: 100%
+`
 
 export default App;
