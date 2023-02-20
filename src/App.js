69d9a4185e1b88891858b5875259cbb7c472e470
Changhwan Kim <kimch061279@gmail.com>
2023-02-20 15:43:15 +0900

    wa!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import styled from "styled-components"
+import shiba from "./img/title.png";
 
 const App = () => {
   return(
@@ -14,25 +15,24 @@ const MainContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap
 `;
 
 const LeftContainer = styled.div`
-  margin: 0px;
   width: 400px;
-  border: 0;
   height: 100vh;
   background-color: #59d7eb;
   background-size: cover;
   display: flex;
   justify-content: space-evenly;
-  align-items: flex-end
+  align-items: flex-end;
+  position: fixed;
 `;
 
 const RightContainer = styled.div`
   width: 100%;
+  margin-left: 400px;
+  padding: 100px 200px;
   display: fixed;
-  justify-content: space-around;
   flex-wrap: wrap;
   flex-direction: row;
 `;
@@ -43,27 +43,11 @@ const LeftText = styled.p`
   color: rgb(0, 0, 0);
 `;
 
-const RightText = styled.p`
-  font-size: 30px;
-  margin: 20px;
-  color: rgb(0, 0, 0);
-`;
-
 const OutUpPost = styled.div`
   width:400px;
   height:200px;
   display:inline-block;
   background-color: white;
-  margin: 100px; 50px; 50px; 50px;
-`;
-const InUpPost = styled.div`
-  width:400px;
-  height:150px;
-  display:inline-block;
-  background-color: blue;
-  display: flex;
-  text-align: center;
-  justify-content: center;
 `;
 
 const OutDownPost = styled.div`
@@ -71,8 +55,17 @@ const OutDownPost = styled.div`
   height:250px;
   display:inline-block;
   background-color: white;
-  margin: 100px; 50px; 50px; 50px;
 `;
+
+const InUpPost = styled.div`
+  width:400px;
+  height:150px;
+  display:inline-block;
+  background-color: #7eaef2;
+  display: flex;
+  text-align: center;
+`;
+
 const InDownPost = styled.div`
   width:250px;
   height:250px;
@@ -81,6 +74,29 @@ const InDownPost = styled.div`
   display: flex;
   text-align: center;
   justify-content: center;
+`;
+
+const UpPostBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
+
+const DownPostBox = styled.div`
+  
+`;
+
+const UpPostTitle = styled.div`
+  margin: 30px 0px 0px 40px;
+  font-size: large;
+  font-weight: bold;
+  height: 100px;
+`;
+
+const UpPostDate = styled.div`
+  margin: 0px 0px 0px 58px;
+  font-size: small;
+  font-weight: lighter;
 `;
 
 const Left = () => {
@@ -97,14 +113,18 @@ const Left = () => {
 const Right = () => {
   return (
     <RightContainer>
-      <OutUpPost><InUpPost>안녕하세요</InUpPost></OutUpPost>
-      <OutUpPost><InUpPost>이거 왜 안되는걸까요</InUpPost></OutUpPost>
-      <OutUpPost><InUpPost>진짜 몰루겠어요</InUpPost></OutUpPost>
-      <OutUpPost><InUpPost>ㅠㅠㅠㅠ</InUpPost></OutUpPost>
-      <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
-      <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
-      <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
-      <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
+      <UpPostBox>
+        <OutUpPost><InUpPost><img src={shiba}/><UpPostTitle>안녕하세요</UpPostTitle><UpPostDate>2023.02.20</UpPostDate></InUpPost></OutUpPost>
+        <OutUpPost><InUpPost><img src={shiba}/><UpPostTitle>이 글은</UpPostTitle><UpPostDate>2023.02.20</UpPostDate></InUpPost></OutUpPost>
+        <OutUpPost><InUpPost><img src={shiba}/><UpPostTitle>테스트 글입니다</UpPostTitle><UpPostDate>2023.02.20</UpPostDate></InUpPost></OutUpPost>
+        <OutUpPost><InUpPost><img src={shiba}/><UpPostTitle>감사합니다</UpPostTitle><UpPostDate>2023.02.20</UpPostDate></InUpPost></OutUpPost>
+      </UpPostBox>
+      <DownPostBox>
+        <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
+        <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
+        <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
+        <OutDownPost><InDownPost>sans1</InDownPost></OutDownPost>
+      </DownPostBox>
     </RightContainer>
   );
 };
