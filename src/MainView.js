8597202a0c30412f02af3217042e2f1ca4bd5219
Changhwan Kim <kimch061279@gmail.com>
2023-02-23 18:33:43 +0900

    final?

--- src/MainView.js
+++ src/MainView.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
-import { Outlet } from "react-router-dom";
 import shiba from "./img/title.png";
 import large from "./img/large.png";
+import eagle from "./img/eagle.png";
+import thanks from "./img/thanks.gif";
 import {OutDownPost, InDownPost, DownPostBox, DownPostTitle} from "./Down_area";
 import {OutUpPost, InUpPost, UpPostBox, UpPostTitle, UpPostDate} from "./Up_area";
 import { Link } from "react-router-dom";
@@ -28,6 +29,7 @@ const SubView = () => {
           <RightPostTitle>안녕하세요</RightPostTitle>
           <RightPostDate>작성일 : 2023.02.20</RightPostDate>
           <RightPost>이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고 지금은 당신에게로 옮겨진 이 편지는 4일 안에 당신 곁을 떠나야 합니다. 이 편지를 포함해서 7통을 행운이 필요한 사람에게 보내 주셔야 합니다. 복사를 해도 좋습니다. 혹 미신이라 하실지 모르지만 사실입니다. 영국에서 HGXWCH이라는 사람은 1930년에 이 편지를 받았습니다. 그는 비서에게 복사해서 보내라고 했습니다. 며칠 뒤에 복권이 당첨되어 20억을 받았습니다. 어떤 이는 이 편지를 받았으나 96시간 이내 자신의 손에서 떠나야 한다는 사실을 잊었습니다. 그는 곧 사직되었습니다. 나중에야 이 사실을 알고 7통의 편지를 보냈는데 다시 좋은 직장을 얻었습니다. 미국의 케네디 대통령은 이 편지를 받았지만 그냥 버렸습니다. 결국 9일 후 그는 암살당했습니다. 기억해 주세요. 이 편지를 보내면 7년의 행운이 있을 것이고 그렇지 않으면 3년의 불행이 있을 것입니다. 그리고 이 편지를 버리거나 낙서를 해서는 절대로 안됩니다. 7통입니다. 이 편지를 받은 사람은 행운이 깃들것입니다. 힘들겠지만 좋은게 좋다고 생각하세요. 7년의 행운을 빌면서...</RightPost>
+          <RightPostHash>#행운의편지</RightPostHash>
         </RightContainer>
       </MainContainer>
       </>
@@ -40,8 +42,43 @@ const SubView = () => {
       <MainContainer>
         <Left/>
         <RightContainer>
-          <p>이 글은</p>
-          <p>영어로 eagle입니다.</p>
+          <RightPostTitle>이 글은</RightPostTitle>
+          <RightPostDate>작성일 : 2023.02.20</RightPostDate>
+          <RightPost>영어로 Eagle이라고 씁니다</RightPost>
+          <img src={eagle}></img>
+          <RightPostHash>#이글</RightPostHash>
+        </RightContainer>
+      </MainContainer>
+      </>
+    );
+  };
+
+  if (params.postID === "0003") {
+    return (
+      <>
+      <MainContainer>
+        <Left/>
+        <RightContainer>
+          <RightPostTitle>테스트 글입니다</RightPostTitle>
+          <RightPostDate>작성일 : 2023.02.20</RightPostDate>
+          <RightPost>500 Internal Server Error</RightPost>
+          <RightPostHash>#Maintenance</RightPostHash>
+        </RightContainer>
+      </MainContainer>
+      </>
+    );
+  };
+
+  if (params.postID === "0004") {
+    return (
+      <>
+      <MainContainer>
+        <Left/>
+        <RightContainer>
+          <RightPostTitle>감사합니다</RightPostTitle>
+          <RightPostDate>작성일 : 2023.02.20</RightPostDate>
+          <img src={thanks}></img>
+          <RightPostHash>#THX</RightPostHash>
         </RightContainer>
       </MainContainer>
       </>
@@ -57,14 +94,21 @@ const RightPostTitle = styled.p`
 
 const RightPostDate = styled.p`
   font-size: 19px;
-  margin: 15px 0px 0px 0px;
+  margin: 15px 0px 10px 0px;
   color: rgb(0, 0, 0);
 `;
 
 const RightPost = styled.p`
   font-size: 25px;
-  margin: 70px 0px 0px 0px;
+  margin: 70px 0px 20px 0px;
   color: rgb(0, 0, 0);
+`;
+
+const RightPostHash = styled.p`
+  font-size: 15px;
+  margin: 20px 0px 0px 0px;
+  color: rgb(0, 0, 0);
+  font-weight: lighter
 `;
 
 const MainContainer = styled.div`
